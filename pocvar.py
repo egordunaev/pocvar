b147c2794e1df1cc57cfedf396bdcab822d2d473
Egor <egodunaev@gmail.com>
2021-03-05 22:56:55 +0300

    Updated search method and general work. TODO add saving

--- pocvar.py
+++ pocvar.py
@@ -1,44 +1,105 @@
 import sys
-import io
 import numpy as np
-import cv2 as cv
+import cv2
+from cv2 import aruco
 import sys
-import argparse
-# from matplotlib import pyplot as plt
 
 
 class POCVAR():
-    def __init__(self, markers = None, images = None, video = None, match_count = 10):
-        self.markers = read_markers(markers)
-        self.images = read_images(images)
+    def __init__(self,
+                 markers=None,
+                 images=None,
+                 video=None,
+                 display_video=True,
+                 save_video=False):
+        self.marker = read_markers(markers)
+        self.image = read_images(images)
         self.video = read_video(video)
-        self.match_count = match_count
-    
-    def find_and_replace(self):
-        pass
+        self.display_video = display_video
+        self.save_video = save_video
+
+        #if self.save_video is True:
+            #self.video_edit = np.array()
+
+    def augment_video(self):
+        try:
+            total_frames = 0
+            while self.video.isOpened():
+                success, frame = self.video.read()
+                if not success:
+                    print("Can't read video...")
+                    break
+                total_frames += 1
+                print(f"Processing frame {total_frames}...")
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
+                arucoParameters = aruco.DetectorParameters_create()
+                corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=arucoParameters)
+                if np.all(ids != None):
+                    display = aruco.drawDetectedMarkers(frame, corners)
+                    x1 = (corners[0][0][0][0], corners[0][0][0][1])
+                    x2 = (corners[0][0][1][0], corners[0][0][1][1])
+                    x3 = (corners[0][0][2][0], corners[0][0][2][1])
+                    x4 = (corners[0][0][3][0], corners[0][0][3][1])
+
+                    im_dst = frame
+                    size = self.image.shape
+                    pts_dst = np.array([x1, x2, x3, x4])
+                    pts_src = np.array([[0, 0], [size[1] - 1, 0], [size[1] - 1, size[0] - 1], [0, size[0] - 1]], dtype=float)
+
+                    h, status = cv2.findHomography(pts_src, pts_dst)
+                    temp = cv2.warpPerspective(self.image, h, (im_dst.shape[1], im_dst.shape[0]))
+                    cv2.fillConvexPoly(im_dst, pts_dst.astype(int), 0, 16)
+                    im_dst = im_dst + temp
+                    if self.display_video: cv2.imshow("Display", im_dst)
+                else:
+                    display = frame
+                    if self.display_video: cv2.imshow("Display", display)
+                if self.display_video: cv2.waitKey()
+        except Exception as error:
+            print(f"An error occured: {error}")
     
 def read_markers(marker_path):
     try:
-        print(f"Reading marker on {marker_path[1:]}...")
-        return cv.imread(marker_path)
-    except Exception:
-        print("No marker.")
+        print(f"Reading marker on {marker_path}...")
+        return cv2.imread(marker_path)
+    except Exception as error:
+        print(f"An error occured: {error}")
     
 def read_images(image_path):
     try:
-        print(f"Reading replacement image on {image_path[1:]}...")
-        return cv.imread(image_path)
-    except Exception:
-        print("No image.")
+        print(f"Reading replacement image on {image_path}...")
+        return cv2.imread(image_path)
+    except Exception as error:
+            print(f"An error occured: {error}")
 
 def read_video(video_path):
     try:
         print(f"Reading video on {video_path}...")
-        return cv.VideoCapture(video_path)
-    except Exception:
-        print("No video.")
+        return cv2.VideoCapture(video_path)
+    except Exception as error:
+            print(f"An error occured: {error}")
     
 
 if __name__ == "__main__":
-    pocvar = POCVAR("./markers/marker.jpg", "./img/img.jpg", "/video/marker-testing.mp4")
-    print(f"Pocvar status: marker read: {pocvar.markers.any()}; img read: {pocvar.images.any()}; video read: {bool(pocvar.video)}")
+    arguments = sys.argv[1:]
+    marker_path, img_path, video_path = "", "", ""
+    try:
+        if arguments[0] == "-m":
+            marker_path = arguments[1]
+        if arguments[2] == "-i":
+            img_path = arguments[3]
+        if arguments[4] == "-v":
+            video_path = arguments[5]
+        if arguments[6] == "-d":
+            if arguments[7] == "True":
+                display_video_bool = True
+            else: display_video_bool = False
+        if arguments[8] == "-s":
+            if arguments[9] == "True":
+                save_video_bool = True
+            else: save_video_bool = False
+    except Exception:
+        print(f"An error occured parsing your input.")
+    pocvar = POCVAR(marker_path, img_path, video_path, display_video=display_video_bool, save_video=save_video_bool)
+    pocvar.augment_video()
